475115a8065a434d4f46723b024622cdf48b7a51
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-01-13 17:09:12 +0200

    2.3. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,5 @@
 package ru.job4j;
+
 /**
  * Calculate.
  *
@@ -7,21 +8,23 @@ package ru.job4j;
  * @since 1.0
  */
 public class Calculate {
+    /**
+     * This is my first class
+     */
+    public static void main(String[] args) {
 /**
-* This is my first class
-*/
-	public static void main(String[] args) {
-/**
-* This is the most important method in Java
-*/
-	System.out.println("I not will write: Hello World");
-	}
-	/**
-	 * Method echo.
-	 * @param name Your name.
-	 * @return Echo plus your name.
-	 */
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+ * This is the most important method in Java
+ */
+        System.out.println("I not will write: Hello World");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/calculater/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculater/CalculatorTest.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
-
+/**
+ * Test.
+ *
+ * @author Alexandar Vysotskiy (alexandar.vysotskiy@gmail.com)
+ * @version 1.0
+ * @since 13.01.2018
+ */
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -12,6 +18,30 @@ public class CalculatorTest {
         calc.add(1D, 1D);
         double result = calc.getResult();
         double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    public void whenSubtractOneVychestOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    public void whenDivOneMinusOneThenZero() {
+        Calculator calc = new Calculator();
+        calc.div(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+
+    public void whenMultipleOneUmnoshitOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.multiple(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
         assertThat(result, is(expected));
     }
 }
