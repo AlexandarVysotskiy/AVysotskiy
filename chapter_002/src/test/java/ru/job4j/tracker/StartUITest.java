a3ed878308635ca47813d09f3f690be07d46e922
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-03-03 22:31:25 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,8 +2,17 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 
 /**
  * @author Aleksandr Vysotskiiy (Aleksandr.vysotskiiy@gmail.com)
@@ -11,7 +20,29 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 
+
 public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final String menu = "Выберети пункт меню:\r\n"
+            + "0. Добавление новой заявки\r\n"
+            + "1. Показать все заявки\r\n"
+            + "2. Редактирование заявки\r\n"
+            + "3. Удаление заявки\r\n"
+            + "4. Поиск заявки по ID\r\n"
+            + "5. Поиск заявки по имени\r\n"
+            + "6. Выход из программы";
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();     // создаём Tracker
@@ -21,17 +52,21 @@ public class StartUITest {
     }
 
     @Test
-    public void whenUpdateThenTrackerHasUpdatedValue() {
-        // создаём Tracker
+    public void whenShowAllThenAllItems() {
         Tracker tracker = new Tracker();
-        //Напрямую добавляем заявку
-        Item item = tracker.add(new Item("name", "decs", 123L));
-        //создаём StubInput с последовательностью действий
-        Input input = new StubInput(new String[]{"1", item.getId(), "6"});
-        // создаём StartUI и вызываем метод init()
-        new StartUI(input, tracker).init();
-        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        assertThat(tracker.findById(item.getId()).getName(), is("name"));
+        Item item = tracker.add(new Item("test name", "decs,", 123L));
+        Item[] items = tracker.findAll();
+        String expected = new StringBuilder()
+                .append(this.menu).append(System.lineSeparator()).
+                        append("Список всех заявок:" + items.length).append(System.lineSeparator()).
+                        append("Имя: ").append("test name").append(" ID: ").append(item.getId()).append(System.lineSeparator()).
+                        append(this.menu).append(System.lineSeparator()).toString();
+        Input input = new StubInput(new String[]{"1", "6"});
+        StartUI start = new StartUI(input, tracker);
+        this.loadOutput();
+        start.init();
+        assertThat(new String(this.out.toByteArray()), is(expected));
+        this.backOutput();
     }
 
     @Test
