13b92da7c4196dc0514ab06e85e04927dc22cff3
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-03-08 20:26:38 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -34,8 +34,20 @@ public class StubInput implements Input {
     public String ask(String question) {
         return this.value[this.position++];
     }
-    public int ask(String question, int[] range){
-     //   throw new UnsupportedOperationException("Unsupported Operation Exception");
-    return -1;
+    @Override
+    public int ask(String question, int[] range) {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Введите число");
+        }
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,17 +1,26 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input input;
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
     public int ask(String question, int[] range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
-            } catch (MenuOutException moe) {
-                System.out.println("Выберите пункт меню из списка.");
             } catch (NumberFormatException nfe) {
                 System.out.println("Выберите пункт меню повторно.");
+            } catch (MenuOutException moe) {
+                System.out.println("Введите номер из пункта меню");
             }
         } while (invalid);
         return value;
