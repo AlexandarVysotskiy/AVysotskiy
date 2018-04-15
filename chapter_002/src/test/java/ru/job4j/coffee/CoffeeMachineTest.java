efeb27087e697c1b5428ac0ff92a289433b07281
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-04-15 21:40:36 +0300

    Кофемашина (посчитать сдачу). [#34741]

---
+++ chapter_002/src/test/java/ru/job4j/coffee/CoffeeMachineTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.coffee;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CoffeeMachineTest {
+    @Test
+    public void changesTest() {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        int[] result = coffeeMachine.changes(62, 100);
+        int[] expected = {10, 10, 10, 5, 2, 1};
+        assertThat(expected, is(result));
+    }
+}
