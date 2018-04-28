9f34504aaf3d81014c5bc8330352f665cdb715ac
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-04-28 07:08:24 +0300

    Исправление ошибок по mvn validate

--- chapter_002/src/test/java/ru/job4j/strategy/strategyTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/strategyTest.java
@@ -1,41 +1,39 @@
 package ru.job4j.strategy;
 
+/**
+ * @author Aleksandr Vysotskiiy (Aleksandr.vysotskiiy@gmail.com)
+ * @version 1.0
+ * @since 0.1
+ */
+
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StrategyTest {
-    /**
-     * @version $Id$
-     * @since 0.1
-     */
     public static String newline = System.getProperty("line.separator");
 
     @Test
     public void whenDrawSquare() {
         Square square = new Square();
         assertThat(square.draw(), is(new StringBuilder()
-                        .append("+ + + +" + newline)
-                        .append("+     +" + newline)
-                        .append("+     +" + newline)
-                        .append("+ + + +" + newline)
-                        .toString()
-                )
-        );
+                .append("+ + + +" + newline)
+                .append("+     +" + newline)
+                .append("+     +" + newline)
+                .append("+ + + +" + newline)
+                .toString()));
     }
 
     @Test
     public void whenDrawTriangle() {
         Triangle triangle = new Triangle();
         assertThat(triangle.draw(), is(new StringBuilder()
-                        .append("   +   " + newline)
-                        .append("  + +  " + newline)
-                        .append(" + + + " + newline)
-                        .append("+ + + +")
-                        .toString()
-                )
-        );
+                .append("   +   " + newline)
+                .append("  + +  " + newline)
+                .append(" + + + " + newline)
+                .append("+ + + +")
+                .toString()));
     }
 }
 
