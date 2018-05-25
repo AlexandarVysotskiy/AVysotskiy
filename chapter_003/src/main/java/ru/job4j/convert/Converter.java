0c9d470fa69fef1231b141c11a235b0d9b0bcca2
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-05-25 07:07:00 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152] Исправленное.

--- chapter_003/src/main/java/ru/job4j/convert/Converter.java
+++ chapter_003/src/main/java/ru/job4j/convert/Converter.java
@@ -7,7 +7,7 @@ import java.util.NoSuchElementException;
  * Converter class
  *
  * @author Aleksandr Vysotskiy
- * @verion 1.0
+ * @verion 1.5
  * @since 18.05.18
  */
 
@@ -16,37 +16,44 @@ public class Converter {
      * This is a method for runs through iterators which contain elements of type Integer.
      *
      * @param it - Iterator containing iterators.
-     * @return
+     * @return Iterator containing Integer.
      */
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         return new Iterator<Integer>() {
             Iterator<Integer> first;
             Iterator<Integer> second;
 
-            private void check() {
-                if (first != null && first.hasNext()) {
-                    return;
-                }
-                first = null;
-                while (it.hasNext()) {
-                    second = it.next();
-                    if (second.hasNext()) {
-                        first = second;
-                        break;
-                    }
-                }
-            }
-
+            /**
+             * This is a method hasNext.
+             *
+             * @return true if there is next object, other false.
+             */
             @Override
             public boolean hasNext() {
-                check();
-                return first != null;
+                boolean result = false;
+                if (first != null && first.hasNext()) {
+                    result = true;
+                } else {
+                    while (it.hasNext()) {
+                        second = it.next();
+                        if (second.hasNext()) {
+                            first = second;
+                            result = true;
+                            break;
+                        }
+                    }
+                }
+                return result;
             }
 
+            /**
+             * This is a method next;
+             *
+             * @return next object in iteration.
+             */
             @Override
             public Integer next() {
-                check();
-                if (first == null) {
+                if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
                 return first.next();
