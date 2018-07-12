249acfc30f32e273f827c5dee4b1ca5e91c8f7a3
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-07-12 21:20:23 +0300

    Перекрывать и equals и hashCode

--- chapter_003/src/main/java/ru/job4j/model/User.java
+++ chapter_003/src/main/java/ru/job4j/model/User.java
@@ -16,14 +16,14 @@ public class User {
         this.birthday = birthday;
     }
 
-//    @Override
-//    public int hashCode() {
-//        int result = 17;
-//        result = 31 * result + name.hashCode();
-//        result = 31 * result + children;
-//        result = 31 * result + birthday.hashCode();
-//        return result;
-//    }
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + children;
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
 
     @Override
     public boolean equals(Object o) {
