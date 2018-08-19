a3cc4634bd92c54cda419b495eddb66872c9bc42
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-08-19 17:41:51 +0300

    Реализовать механизм программной остановки потока. [#1019]

--- chapter_004/src/main/java/ru/job4j/pingpong/PingPong.java
+++ chapter_004/src/main/java/ru/job4j/pingpong/PingPong.java
@@ -16,10 +16,14 @@ public class PingPong extends Application {
         Group group = new Group();
         Rectangle rect = new Rectangle(50, 100, 100, 100);
         group.getChildren().add(rect);
-        new Thread(new RectangleMove(rect)).start();
+        Thread move = new Thread(new RectangleMove(rect));
+        move.start();
         stage.setScene(new Scene(group, limitX, limitY));
         stage.setTitle(JOB4J);
         stage.setResizable(false);
         stage.show();
+        stage.setOnCloseRequest(
+                event -> move.interrupt()
+        );
     }
 }
