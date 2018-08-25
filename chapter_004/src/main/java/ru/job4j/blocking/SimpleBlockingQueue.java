0da3043a4ec973f1d483837276199ab63bf2821c
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-08-25 20:06:32 +0300

    Реализовать шаблон Producer Consumer

---
+++ chapter_004/src/main/java/ru/job4j/blocking/SimpleBlockingQueue.java
@@ -0,0 +1,57 @@
+package ru.job4j.blocking;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Класс состоит их двух методов:
+ * Первый добавляет элементы в коллекцию если очередь не полна.
+ * Второй извлекает элементы из коллекции если очередь не полна.
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<E> {
+
+    @GuardedBy("this")
+    private Queue<E> queue = new LinkedList<>();
+
+    private E size;
+
+    public SimpleBlockingQueue(E size) {
+        this.size = size;
+    }
+
+    public synchronized void offer(E value) {
+        if (queue.size() > (int) size) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Исключения типа InterruptedException перехвачено");
+            }
+        } else {
+            queue.offer(value);
+            notify();
+        }
+    }
+
+    public synchronized E poll() {
+        E result = null;
+        if (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Исключения типа InterruptedException перехвачено");
+            }
+        } else {
+            result = queue.poll();
+            notify();
+        }
+        return result;
+    }
+
+    public int getSizeQueue() {
+        return queue.size();
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.blocking;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+    SimpleBlockingQueue<Integer> blocking = new SimpleBlockingQueue<>(7);
+    private Thread producer;
+    private Thread consumer;
+
+    @Before
+    public void when() {
+        producer = new Thread() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (i < 6) {
+                    blocking.offer(i);
+                    i++;
+                }
+            }
+        };
+
+        consumer = new Thread() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (i < 6) {
+                    assertThat(blocking.poll(), is(i));
+                    i++;
+                }
+            }
+        };
+    }
+
+    @Test
+    public void start() throws InterruptedException {
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+    }
+}
