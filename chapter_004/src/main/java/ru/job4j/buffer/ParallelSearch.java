e3cbcee35da16e1d91dbfb22d75d5406d1ed72f6
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-09-02 17:51:53 +0300

    Обеспечить остановку потребителя

---
+++ chapter_004/src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,31 @@
+package ru.job4j.buffer;
+
+import ru.job4j.blocking.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(9);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.setDaemon(true);
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
