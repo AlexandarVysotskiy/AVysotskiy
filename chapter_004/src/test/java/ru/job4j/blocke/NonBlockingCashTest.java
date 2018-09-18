e872167161d455ae1063f58192532f32948e669f
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-09-18 13:36:41 +0300

    Неблокирующий кеш. Добавлен тест на OptimisticException.

--- chapter_004/src/test/java/ru/job4j/blocke/NonBlockingCashTest.java
+++ chapter_004/src/test/java/ru/job4j/blocke/NonBlockingCashTest.java
@@ -1,9 +1,12 @@
 package ru.job4j.blocke;
 
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.blocking.Model;
 import ru.job4j.blocking.NonBlockingCash;
 import ru.job4j.blocking.OptimisticException;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertNull;
@@ -11,11 +14,18 @@ import static org.hamcrest.core.Is.is;
 
 public class NonBlockingCashTest {
     Model first = new Model("1", "First");
-    NonBlockingCash nonBlockingCash = new NonBlockingCash();
+    Model second = new Model("2", "Second");
+    NonBlockingCash nonBlockingCash;
+
+    @Before
+    public void addElement() {
+        nonBlockingCash = new NonBlockingCash();
+        nonBlockingCash.add(1, first);
+        nonBlockingCash.add(2, second);
+    }
 
     @Test
     public void deleteTest() {
-        nonBlockingCash.add(1, first);
         assertThat(nonBlockingCash.get(1), is(first));
         nonBlockingCash.delete(1);
         assertNull(nonBlockingCash.get(1));
@@ -23,7 +33,6 @@ public class NonBlockingCashTest {
 
     @Test
     public void updateTest() {
-        nonBlockingCash.add(1, first);
         Model newModel = new Model("2", "Second");
         try {
             nonBlockingCash.update(1, newModel);
@@ -32,5 +41,39 @@ public class NonBlockingCashTest {
         }
         assertThat(nonBlockingCash.get(1), is(newModel));
         assertThat(nonBlockingCash.get(1).getVersion(), is(1));
+    }
+
+    @Test
+    public void whenExceptionOptimisticException() throws InterruptedException {
+        AtomicReference<Exception> ex = new AtomicReference<>();
+        Thread threadOne = new Thread(
+                () -> {
+                    Model model = nonBlockingCash.get(1);
+                    model.change("Вася");
+                    try {
+                        Thread.sleep(200);
+                        nonBlockingCash.update(1, model);
+                    } catch (InterruptedException ie) {
+                        ie.printStackTrace();
+                    }
+                }
+        );
+        Thread threadTwo = new Thread(
+                () -> {
+                    Model model = nonBlockingCash.get(1);
+                    model.change("Илон");
+                    try {
+                        Thread.sleep(1000);
+                        nonBlockingCash.update(1, model);
+                    } catch (OptimisticException | InterruptedException ie) {
+                        ex.set(ie);
+                    }
+                }
+        );
+        threadOne.start();
+        threadTwo.start();
+        threadOne.join();
+        threadTwo.join();
+        assertThat(ex.get().getMessage(), is("OptimisticException"));
     }
 }
