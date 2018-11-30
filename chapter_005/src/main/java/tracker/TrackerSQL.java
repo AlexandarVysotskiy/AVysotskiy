2da5aa220cc6be85761bfe768e96cecee7820b71
AlexandarVysotskiy <alexandar.vysotskiy@gmail.com>
2018-11-30 17:27:47 +0300

    Трекер SQL. Закрытие ресурсов через try-with-resources.

--- chapter_005/src/main/java/tracker/TrackerSQL.java
+++ chapter_005/src/main/java/tracker/TrackerSQL.java
@@ -34,9 +34,9 @@ public class TrackerSQL implements ITracker, Closeable {
     }
 
     /**
-     * Метод устанавливает соединение с БД.
+     * Set connection by DB.
      *
-     * @return удалось/не удалось.
+     * @return get/don't get.
      */
     public boolean init() {
         try {
@@ -83,13 +83,13 @@ public class TrackerSQL implements ITracker, Closeable {
     private boolean isItemsTableExist() throws SQLException {
         boolean result = false;
         String query = "SELECT COUNT(t.table_name) FROM information_schema.tables t WHERE t.table_schema='public' AND t.table_name = 'items'";
-        PreparedStatement ps = this.connection.prepareStatement(query);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            result = rs.getInt("count") == 1;
+        try (PreparedStatement ps = this.connection.prepareStatement(query)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                result = rs.getInt("count") == 1;
+            }
+            rs.close();
         }
-        rs.close();
-        ps.close();
         return result;
     }
 
@@ -103,13 +103,13 @@ public class TrackerSQL implements ITracker, Closeable {
     private boolean itemsTableCorrect() throws SQLException {
         boolean result = false;
         String query = String.format("SELECT SUM(nessesaryColumnCount) nessesaryColumnCount,SUM(unnesesarryColumnCount) unnesesarryColumnCount \nFROM (SELECT SUM(VT.count) nessesaryColumnCount, 0 unnesesarryColumnCount \n\t  from (SELECT COUNT(c.column_name) count FROM information_schema.columns c \n\t\t\tWHERE c.table_schema='public' AND c.table_name = 'items' \n\t\t\tAND c.column_name = 'id' AND c.data_type = 'integer' \n\t\t\tUNION ALL SELECT COUNT(c.column_name) FROM information_schema.columns c \n\t\t\tWHERE c.table_schema='public' AND c.table_name = 'items' AND c.column_name = 'name' \n\t\t\tAND c.data_type = 'character varying' UNION ALL SELECT COUNT(c.column_name) \n\t\t\tFROM information_schema.columns c WHERE c.table_schema='public' AND c.table_name = 'items' \n\t\t\tAND c.column_name = 'description' AND c.data_type = 'character varying' \n\t\t\tUNION ALL SELECT COUNT(c.column_name) FROM information_schema.columns c \n\t\t\tWHERE c.table_schema='public' AND c.table_name = 'items' AND c.column_name = 'created' \n\t\t\tAND c.data_type = 'bigint') VT UNION ALL SELECT 0, COUNT  (c.column_name) FROM information_schema.columns c \n\t  WHERE c.table_schema='public' AND c.table_name = 'items' AND NOT c.column_name IN ('id','name','description','created') \n\t  AND c.is_nullable = 'NO') VT");
-        PreparedStatement ps = this.connection.prepareStatement(query);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            result = rs.getInt("nessesaryColumnCount") == 4 && rs.getInt("unnesesarryColumnCount") == 0;
+        try (PreparedStatement ps = this.connection.prepareStatement(query);) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                result = rs.getInt("nessesaryColumnCount") == 4 && rs.getInt("unnesesarryColumnCount") == 0;
+            }
+            rs.close();
         }
-        rs.close();
-        ps.close();
         return result;
     }
 
@@ -121,8 +121,9 @@ public class TrackerSQL implements ITracker, Closeable {
     private void renameExistingTable() throws SQLException {
         String query = "ALTER TABLE items DROP CONSTRAINT \"PK_items_id\";\n"
                 + "ALTER TABLE items RENAME TO items_old";
-        PreparedStatement ps = this.connection.prepareStatement(query);
-        ps.execute();
+        try (PreparedStatement ps = this.connection.prepareStatement(query)) {
+            ps.execute();
+        }
     }
 
     /**
@@ -132,8 +133,9 @@ public class TrackerSQL implements ITracker, Closeable {
      */
     private void createItemsTable() throws SQLException {
         String query = String.format("CREATE TABLE public.items\n(\n  id serial,\n  name character varying,\n  description character varying,\n  created bigint,\n  CONSTRAINT \"PK_items_id\" PRIMARY KEY (id)\n)");
-        PreparedStatement ps = this.connection.prepareStatement(query);
-        ps.execute();
+        try (PreparedStatement ps = this.connection.prepareStatement(query)) {
+            ps.execute();
+        }
     }
 
     /**
@@ -145,8 +147,7 @@ public class TrackerSQL implements ITracker, Closeable {
     @Override
     public Item add(Item item) {
         String query = "INSERT INTO public.items(name, description, created) VALUES (?,?,?);";
-        try {
-            PreparedStatement ps = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        try (PreparedStatement ps = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, item.getName());
             ps.setString(2, item.getDescription());
             ps.setLong(3, item.getCreate());
@@ -172,14 +173,12 @@ public class TrackerSQL implements ITracker, Closeable {
     @Override
     public void replace(String id, Item item) {
         String query = String.format("UPDATE public.items SET name = ?,description = ?,created = ?WHERE id = ?");
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, item.getName());
             ps.setString(2, item.getDescription());
             ps.setLong(3, item.getCreate());
             ps.setInt(4, Integer.valueOf(id));
             ps.executeUpdate();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -193,11 +192,9 @@ public class TrackerSQL implements ITracker, Closeable {
     @Override
     public void delete(String id) {
         String query = "DELETE FROM public.items where id = ?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setInt(1, Integer.parseInt(id));
             ps.executeUpdate();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -208,10 +205,8 @@ public class TrackerSQL implements ITracker, Closeable {
      */
     public void deleteAll() {
         String query = "DELETE FROM public.items";
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.executeUpdate();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -224,15 +219,13 @@ public class TrackerSQL implements ITracker, Closeable {
     public ArrayList<Item> findAll() {
         ArrayList<Item> result = new ArrayList<>();
         String query = "Select id, name, description, created FROM public.items";
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 result.add(new Item(rs.getString("id"), rs.getString("name"),
                         rs.getString("description"), rs.getLong("created")));
             }
             rs.close();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -249,8 +242,7 @@ public class TrackerSQL implements ITracker, Closeable {
     public List<Item> findByName(String key) {
         List<Item> result = new ArrayList<>();
         String query = "Select id, name, description, created FROM public.items WHERE name = ?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, key);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
@@ -259,7 +251,6 @@ public class TrackerSQL implements ITracker, Closeable {
 
             }
             rs.close();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -270,8 +261,7 @@ public class TrackerSQL implements ITracker, Closeable {
     public Item findById(String id) {
         Item result = null;
         String query = "select * from items where id = ?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setInt(1, (Integer.parseInt(id)));
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
